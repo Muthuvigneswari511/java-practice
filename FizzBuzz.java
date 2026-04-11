e3b3d7866049cecfa844e92f3ec2cb31cc444634
muthu <muthua8930@gmail.com>
2026-04-11 08:32:29 +0530

    initial commit

---
+++ FizzBuzz.java
@@ -0,0 +1,17 @@
+class FizzBuzz{
+public static void main(String[] args){
+int a=3;
+if(a%3==0 && a%5==0){
+System.out.println("Fizz Buzz");
+}
+else if(a%3==0){
+System.out.println("Fizz");
+}
+else if(a%3==0){
+System.out.println("Buzz");
+}
+else{
+System.out.println("Nothing");
+}
+}
+}
